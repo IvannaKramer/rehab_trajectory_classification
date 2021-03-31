cfe04aa5e8bb1a54fa3a8d5f327ab0ad359e720d
Ivanna Kramer <ivannamyckhal@uni-koblenz.de>
2021-03-31 21:51:27 +0200

    fixed indentation error

--- utility/kimore_preprocessing.py
+++ utility/kimore_preprocessing.py
@@ -122,6 +122,8 @@ def convert_trajectories(npzs_only, normalized, dir_pattern='Raw',
 					for ind in range(_orientations.shape[1], _trajectories.shape[1]):
 						ind_list.append(ind)
 						np.delete(_trajectories, ind_list, axis=1)
+						print('new _trajectories.shape')
+						print(_trajectories.shape)
 
 
 				_trajectories = np.concatenate((_trajectories, _orientations), axis=0)
